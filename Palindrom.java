56140d39e099bfeea5479d85cc6acde36e4df039
DenisAFW <denis.afinogenow@yandex.ru>
2022-11-21 16:05:17 +0300

    изменил отступы

--- Palindrom.java
+++ Palindrom.java
@@ -8,11 +8,8 @@ public class Palindrom {
         Scanner f = new Scanner(System.in);
 
         System.out.printf("Введите текст: ");
-
         String word = f.nextLine();
-
         String[] Chars = word.split("");
-        
         boolean result = CheckWord(Chars);
 
         if(result == true){
@@ -27,7 +24,6 @@ public class Palindrom {
     private static boolean CheckWord(String[] word) {
 
         String[] Check = new String[word.length];
-        
         int count = 0;
 
         for(int i = word.length - 1; i >= 0; i--){
@@ -35,11 +31,8 @@ public class Palindrom {
             Check[count] = word[i];
             count++;
         }
-
         String word1 = String.join("",word);
-
         String Check1 = String.join("", Check);
-
         boolean result = new String(Check1).equals(word1);
 
         return result;           
